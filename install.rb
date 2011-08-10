fcd7f29f4fdc28331ca27da1dfeeb9695d381a1e
Elliot Winkler <elliot.winkler@gmail.com>
2011-08-09 23:02:53 -0600

    Augmenting install.rb to support dry-run and to print the command being executed

--- install.rb
+++ install.rb
@@ -4,13 +4,32 @@
 
 home = File.expand_path(ENV['HOME'])
 
+opts = {}
+ARGV.each do |arg|
+  if arg =~ /^--(.*)$/
+    opts[$1.gsub("-", "_").to_sym] = true
+  end
+end
+
 Dir['*'].each do |file|
-  next if file =~ /install|tags|README/
+  next if file =~ /^\.|install|tags|README/
   target = File.join(home, ".#{file}")
-  if File.exist?(target)
-    puts "skipping #{file}, already installed"
+  full_path = File.expand_path(file)
+  puts "#{full_path} -> #{target}"
+  if opts[:force] or !File.exist?(target)
+    cmd = ["ln", "-sf", "-i", full_path, target]
+    pretty_cmd = cmd.map {|x| x =~ /[ ]/ ? x.inspect : x }.join(" ")
+    if opts[:dry_run]
+      puts "- Would have run: <#{pretty_cmd}>"
+    else
+      puts "- Running: <#{pretty_cmd}>"
+      system(*cmd)
+    end
   else
-    puts "installing #{File.expand_path file} to #{target}"
-    `ln -s -i #{File.expand_path file} #{target}`
+    if opts[:dry_run]
+      puts "- Would have skipped since already installed"
+    else
+      puts "- Skipping #{file}, already installed"
+    end
   end
 end
