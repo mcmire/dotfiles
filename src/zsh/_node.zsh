53e2836b0a6487af3d7f8ce10f1f62a2e848d078
Elliot Winkler <elliot.winkler@gmail.com>
2021-01-15 11:48:24 -0700

    Remove node_modules/.bin from PATH
    When writing build scripts for projects, it is unwise to assume that
    other people have this on their computer. To help correct this
    assumption, remove this altogether.

--- src/zsh/_node.zsh
+++ src/zsh/_node.zsh
@@ -1,7 +1,5 @@
 export NODE_PATH="/usr/local/lib/node:/usr/local/lib/node_modules:./node_modules:$NODE_PATH"
 
-export PATH="./node_modules/.bin:$PATH"
-
 if [[ -o login ]]; then
   export NODE_PATH="$(npm root -g):$NODE_PATH"
 fi
