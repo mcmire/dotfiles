10153215df4ce3fbc63ff0caf8f6866bdb445e21
Elliot Winkler <elliot.winkler@gmail.com>
2021-04-18 14:46:54 -0600

    Fix homebrew init to work on both Intel and Apple Silicon computers

--- src/zsh/_homebrew.zsh
+++ src/zsh/_homebrew.zsh
@@ -1,1 +1,7 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ -d /opt/homebrew ]]; then
+  # Support Apple Silicon computers
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+  # Support Intel computers
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
