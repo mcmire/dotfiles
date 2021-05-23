764c2029e157ac897d65da6478da95bd265906a4
Elliot Winkler <elliot.winkler@gmail.com>
2021-05-23 00:08:36 -0600

    Speed up boot slightly

--- src/zsh/_ruby.zsh
+++ src/zsh/_ruby.zsh
@@ -1,1 +1,5 @@
-CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl) --without-tcl --without-tk"
+if ! [[ -f /tmp/brew-openssl-dir ]]; then
+  brew --prefix openssl > /tmp/brew-openssl-dir
+fi
+
+CONFIGURE_OPTS="--with-openssl-dir=$(< /tmp/brew-openssl-dir) --without-tcl --without-tk"
