ccfc5ac0811bcf44ddac037fffd56f250cedbda9
Elliot Winkler <elliot.winkler@gmail.com>
2025-10-31 22:51:25 -0600

    Fix Python

--- src/config/nvim-next/ftplugin/python.lua
+++ src/config/nvim-next/ftplugin/python.lua
@@ -1,5 +1,6 @@
 -- Use [PEP8](http://www.python.org/dev/peps/pep-0008/)
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.textwidth = 79
+-- XXX: This seems to be getting set in zsh files, plz fix
+-- vim.o.softtabstop = 4
+-- vim.o.tabstop = 4
+-- vim.o.shiftwidth = 4
+-- vim.o.textwidth = 79
