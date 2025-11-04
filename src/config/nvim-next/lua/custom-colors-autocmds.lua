a8984d7e0378c64023c7c0051975019604915cd3
Elliot Winkler <elliot.winkler@gmail.com>
2025-11-03 23:01:41 -0700

    Remove personal path

--- src/config/nvim-next/lua/custom-colors-autocmds.lua
+++ src/config/nvim-next/lua/custom-colors-autocmds.lua
@@ -2,20 +2,36 @@
 -- Custom colors autocommands
 -- ******************************
 
+--- @generic T : table
+--- @param values T
+--- @return T
+function table.uniq(values)
+  local seen = {}
+  local newtable = {}
+  for _, value in ipairs(values) do
+    if not seen[value] then
+      table.insert(newtable, value)
+      seen[value] = true
+    end
+  end
+  return newtable
+end
+
 -- Source: <https://github.com/neovim/neovim/issues/32116>
 local this_directory = vim.fs.dirname(debug.getinfo(1, 'S').source:sub(2))
-print('This directory: ' .. this_directory)
+
+local custom_colors_file = this_directory .. '/custom-colors.lua'
 
 -- When we're editing a file, new diagnostics and signs may appear and
 -- disappear, and we don't want the sign column popping in and out
 vim.api.nvim_create_autocmd('BufWritePost', {
   desc = 'Reload custom colors when updated',
-  -- At the moment just make this work, we will make this better later.
-  pattern = {
-    this_directory .. '/custom-colors.lua',
-    vim.fn.expand '~' .. '/code/personal/dotfiles/src/config/nvim-next/lua/custom-colors.lua',
+  pattern = table.uniq {
+    custom_colors_file,
+    -- Even if we have the dotfiles repo open,
+    -- `custom_colors_file` will be a symlink, so resolve it
+    vim.fn.resolve(custom_colors_file),
   },
-  -- pattern = './custom-colors.lua',
   group = vim.api.nvim_create_augroup('custom-reload-colors', { clear = true }),
   callback = function()
     -- Hack Lua to "unload" this file
