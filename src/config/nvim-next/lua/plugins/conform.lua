beda928d18ee40a02bee1a7f266d2698e26c9a66
Elliot Winkler <elliot.winkler@gmail.com>
2025-11-06 11:15:03 -0700

    Add Prettier to Conform

--- src/config/nvim-next/lua/plugins/conform.lua
+++ src/config/nvim-next/lua/plugins/conform.lua
@@ -13,7 +13,7 @@ return {
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       -- Disable "format_on_save lsp_fallback" for languages that don't
       -- have a well standardized coding style. You can add additional
@@ -23,18 +23,21 @@ return {
         return nil
       else
         return {
-          timeout_ms = 500,
+          -- Prettier can take a long time for some reason
+          timeout_ms = 2000,
           lsp_format = 'fallback',
         }
       end
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      javascript = { 'prettier' },
+      typescript = { 'prettier' },
+
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
-      --
+
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
   },
 }
