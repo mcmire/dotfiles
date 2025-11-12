17bb38b1af9687758b22dbb168c9ea0b52c1be57
Elliot Winkler <elliot.winkler@gmail.com>
2025-11-12 07:27:09 -0700

    tsserver: When adding imports, calc paths relative to project

--- src/config/nvim/lua/plugins/typescript-tools.lua
+++ src/config/nvim/lua/plugins/typescript-tools.lua
@@ -1,5 +1,11 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  opts = {},
+  opts = {
+    settings = {
+      tsserver_file_preferences = {
+        importModuleSpecifierPreference = 'project-relative',
+      },
+    },
+  },
 }
