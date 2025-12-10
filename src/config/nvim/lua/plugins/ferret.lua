e3866574d0c91fc0b1d0db0c25b179ec29af33db
Elliot Winkler <elliot.winkler@gmail.com>
2025-12-10 12:28:08 -0700

    Sort rg results by path

--- src/config/nvim/lua/plugins/ferret.lua
+++ src/config/nvim/lua/plugins/ferret.lua
@@ -5,6 +5,10 @@ return {
   init = function()
     -- Don't create keymaps; expect :Ack to be typed manually
     vim.g.FerretMap = 0
+    -- Always sort by filepath
+    vim.g.FerretExecutableArguments = {
+      rg = '--vimgrep --no-heading --no-config --max-columns 4096 --sort=path',
+    }
     -- Always show all results
     vim.cmd 'cnoreabbrev Ack Ack!'
   end,
