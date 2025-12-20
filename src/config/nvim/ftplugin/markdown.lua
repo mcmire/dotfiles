9965af11372dccabf9e3ecca5437e513d91e1c53
Elliot Winkler <elliot.winkler@gmail.com>
2025-12-20 00:12:55 -0700

    Fix indentation settings in Markdown files

--- src/config/nvim/ftplugin/markdown.lua
+++ src/config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,7 @@
 -- For some reason, autoindentation isn't enabled in Markdown files
 vim.bo.autoindent = true
+-- Override indentation size
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.textwidth = 80
