89713f98ce8dc87d9284edcdbcaaef3e8f904de5
Elliot Winkler <elliot.winkler@gmail.com>
2026-01-11 22:07:35 -0700

    Fix more things for Termux

--- src/zsh/_homebrew.zsh
+++ src/zsh/_homebrew.zsh
@@ -1,7 +1,7 @@
 if which /opt/homebrew/bin/brew &>/dev/null; then
   # Support Apple Silicon computers
   eval "$(/opt/homebrew/bin/brew shellenv)"
-if which /usr/local/bin/brew &>/dev/null; then
+elif which /usr/local/bin/brew &>/dev/null; then
   # Support Intel computers
   eval "$(/usr/local/bin/brew shellenv)"
 else
--- src/zsh/zoxide.zsh
+++ src/zsh/zoxide.zsh
@@ -1,1 +1,5 @@
-eval "$(zoxide init zsh)"
+if which zoxide &>/dev/null; then
+  eval "$(zoxide init zsh)"
+else
+  echo "It doesn't look like you have zoxide installed. Some features won't work."
+fi
