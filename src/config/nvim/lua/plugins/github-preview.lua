858b4690ed02353ca78a9b079973c12f14b070b7
Elliot Winkler <elliot.winkler@gmail.com>
2026-03-05 12:56:38 -0700

    Add github-preview (keep markdown-preview in case it's useful)

---
+++ src/config/nvim/lua/plugins/github-preview.lua
@@ -0,0 +1,17 @@
+return {
+  'wallpants/github-preview.nvim',
+  cmd = { 'GithubPreviewToggle' },
+  keys = { '<leader>mpt' },
+  opts = {
+    -- config goes here
+  },
+  config = function(_, opts)
+    local gpreview = require 'github-preview'
+    gpreview.setup(opts)
+
+    local fns = gpreview.fns
+    vim.keymap.set('n', '<leader>mpt', fns.toggle)
+    vim.keymap.set('n', '<leader>mps', fns.single_file_toggle)
+    vim.keymap.set('n', '<leader>mpd', fns.details_tags_toggle)
+  end,
+}
