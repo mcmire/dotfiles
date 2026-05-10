7981515e8f74affdd37320196e6682cedd13aac2
Elliot Winkler <elliot.winkler@gmail.com>
2026-05-09 22:17:37 -0600

    Add commented out options for render-markdown plugin

--- src/config/nvim/lua/plugins/render-markdown.lua
+++ src/config/nvim/lua/plugins/render-markdown.lua
@@ -1,4 +1,9 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+  -- opts = {
+  --   enabled = true,
+  --   render_modes = true,
+  --   debounce = 200,
+  -- },
 }
