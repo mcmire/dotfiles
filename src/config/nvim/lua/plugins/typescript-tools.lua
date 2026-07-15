d09dde8503361aad919ecd513940dc3bef98eb66
Elliot Winkler <elliot.winkler@gmail.com>
2026-07-15 11:42:24 -0600

    Update typescript-tools to expose all code actions by default

--- src/config/nvim/lua/plugins/typescript-tools.lua
+++ src/config/nvim/lua/plugins/typescript-tools.lua
@@ -5,6 +5,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {
     settings = {
+      expose_as_code_action = 'all',
       tsserver_file_preferences = {
         importModuleSpecifierPreference = 'project-relative',
       },
